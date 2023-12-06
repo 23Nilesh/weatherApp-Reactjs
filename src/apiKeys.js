4879d1ee1e32127682b4389be65ed6900c8cb0c3
23Nilesh <nilesh.2@iitj.ac.in>
2023-12-06 16:07:08 +0530

    updated code

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "fcaad0ff774cedb5b9261f97b6913ce8",
+  key: "---------",
   base: "https://api.openweathermap.org/data/2.5/",
 };
